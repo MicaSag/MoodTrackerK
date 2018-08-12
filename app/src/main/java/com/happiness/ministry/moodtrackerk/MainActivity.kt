a2deb8fa133f6fd46770bcfbfd111aadbf96916f
Michaël <sagotmickael@neuf.fr>
2018-08-12 18:28:17 +0200

    First variable test

--- app/src/main/java/com/happiness/ministry/moodtrackerk/MainActivity.kt
+++ app/src/main/java/com/happiness/ministry/moodtrackerk/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.happiness.ministry.moodtrackerk
 
+import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Defined Preferences of the application
+        var mPreferences: SharedPreferences
     }
 }
